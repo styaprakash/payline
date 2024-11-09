761df49e7fb5c354210995b4e86d611366f184b9
Satya Prakash Mishra <kali69@MiWiFi-R4CM-srv.stya's>
2024-11-09 22:21:52 +0530

    Imported recoil in the user-app and updated the prisma schema.

--- apps/user-app/app/layout.tsx
+++ apps/user-app/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import { Providers } from "../provider";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -23,9 +24,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={`${geistSans.variable} ${geistMono.variable}`}>
-        {children}
-      </body>
+      <Providers>
+        <body className={inter.className}>{children}</body>
+      </Providers>
     </html>
   );
 }
